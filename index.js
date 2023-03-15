d682569668c374d80680440fa2677ad7787a880e
Florian Stabel <f.stabel@gmx.de>
2023-03-15 19:10:17 +0100

    state-borders and some styling

--- index.js
+++ index.js
@@ -19,27 +19,12 @@ async function getData(url) {
   }
 }
 
-const educationData = await getData(urlEducationData);
-const test = await getEducationData();
-const geoData = await getData(urlGeoData);
-const countiesGeoJson = topojson.feature(geoData, geoData.objects.counties);
-const valueArray = d3.map(educationData, (d) => d.bachelorsOrHigher);
-const minValue = d3.min(educationData, (d) => d.bachelorsOrHigher);
-const maxValue = d3.max(educationData, (d) => d.bachelorsOrHigher);
-const stepCount = 7;
-
-async function getEducationData() {
-  const rawData = await getData(urlEducationData);
+function getEducationData(rawDataArray) {
   const eduDataObj = {};
-  rawData.forEach((d) => (eduDataObj[d.fips] = d));
+  rawDataArray.forEach((d) => (eduDataObj[d.fips] = d));
   return eduDataObj;
 }
 
-console.log(countiesGeoJson.features);
-console.log(test);
-console.log(valueArray);
-
-const legendTickValues = getLegendTickValues(stepCount + 1);
 function getLegendTickValues(count) {
   const step = (maxValue - minValue) / count;
   let value = minValue;
@@ -51,22 +36,36 @@ function getLegendTickValues(count) {
   return valueArray.map((value) => Math.round(value));
 }
 
+// map data
+const educationDataArray = await getData(urlEducationData);
+const educationDataObj = getEducationData(educationDataArray);
+
+const geoData = await getData(urlGeoData);
+const statesGeoJson = topojson.feature(geoData, geoData.objects.states);
+const countiesGeoJson = topojson.feature(geoData, geoData.objects.counties);
+
+const minValue = d3.min(educationDataArray, (d) => d.bachelorsOrHigher);
+const maxValue = d3.max(educationDataArray, (d) => d.bachelorsOrHigher);
+const stepCount = 7;
+
+const legendTickValues = getLegendTickValues(stepCount + 1);
+
 // text-variables
 const title = `United States Educational Attainment`;
 const description = ` Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)
     3%`;
 
+// colorScale
 const colorScale = d3
   .scaleQuantize()
   .domain([minValue, maxValue])
-  .range(d3.schemeGreens[7]);
+  .range(d3.schemeGreens[stepCount]);
 
 // layout-variables
 const width = 1000;
-const height = 650;
-const xPadding = 100;
-const yPadding = 30;
+const height = 620;
 
+const legendPadding = 10;
 const legendWidth = width / 4;
 const legendRect = (legendWidth - 20) / stepCount;
 const legendHeight = legendRect * 3;
@@ -92,13 +91,13 @@ const mousemove = (event, d) => {
   tooltip.transition().duration(200).style("opacity", 0.9);
   tooltip
     .html(
-      `<p>${test[d.id].area_name}, ${test[d.id].state}: ${
-        test[d.id].bachelorsOrHigher
-      }%</p>`
+      `<p>${educationDataObj[d.id].area_name}, ${
+        educationDataObj[d.id].state
+      }: ${educationDataObj[d.id].bachelorsOrHigher}%</p>`
     )
-    .attr("data-education", test[d.id].bachelorsOrHigher)
-    .style("left", a + 50 + "px")
-    .style("top", b + 50 + "px");
+    .attr("data-education", educationDataObj[d.id].bachelorsOrHigher)
+    .style("left", a + "px")
+    .style("top", b + 100 + "px");
 };
 
 // container
@@ -117,26 +116,35 @@ const legend = svg
   .append("svg")
   .attr("x", width - legendWidth - 160)
   .attr("y", 30)
-  .attr("width", legendWidth)
+  .attr("width", legendWidth + 2 * legendPadding)
   .attr("height", legendHeight)
   .attr("id", "legend");
 
 const legendXScale = d3
   .scaleLinear()
   .domain(d3.extent(legendTickValues))
-  .range([10, legendWidth - 10]);
+  .range([legendPadding, legendWidth - legendPadding]);
 
-const legendXAxis = d3
+const legendXAxisGenerator = d3
   .axisBottom(legendXScale)
   .tickValues(legendTickValues)
   .tickFormat((d) => d + "%")
-  .tickSize(15);
+  .tickSize(20);
 
-legend
+const legendXAxis = legend
   .append("g")
-  //   .attr("transform", `translate(0, ${legendRect})`)
-  .attr("id", "legend-x-axis")
-  .call(legendXAxis);
+  .attr("id", "legendXAxis")
+  .call(legendXAxisGenerator);
+
+legendXAxis
+  .selectAll(".tick line")
+  .attr("stroke", "orange")
+  .attr("stroke-width", "2");
+
+legendXAxis
+  .selectAll(".tick text")
+  .attr("font-size", "12")
+  .style("color", "orange");
 
 legend
   .selectAll("rect")
@@ -147,11 +155,15 @@ legend
   .attr("y", 0)
   .attr("width", (d, i) => legendRect - 1)
   .attr("height", (d, i) => legendRect / 3)
-  .attr("fill", (d, i) => colorScale(d));
+  .attr("fill", (d, i) => colorScale(d))
+  .attr("stroke", "orange")
+  .attr("stroke-width", "2");
 
 // svg-map
-svg
-  .append("g")
+const counties = svg.append("g");
+const states = svg.append("g");
+
+counties
   .selectAll("path")
   .data(countiesGeoJson.features)
   .enter()
@@ -159,13 +171,22 @@ svg
   .attr("d", d3.geoPath())
   .attr("data-fips", (d) => d.id)
   .attr("data-education", (d) => {
-    return test[d.id].bachelorsOrHigher;
+    return educationDataObj[d.id].bachelorsOrHigher;
   })
-  .attr("fill", (d) => colorScale(test[d.id].bachelorsOrHigher))
+  .attr("fill", (d) => colorScale(educationDataObj[d.id].bachelorsOrHigher))
   .attr("class", "county")
   .on("mousemove", mousemove)
   .on("mouseleave", mouseleave)
   .on("mouseenter", mouseenter);
+
+states
+  .selectAll("path")
+  .data(statesGeoJson.features)
+  .enter()
+  .append("path")
+  .attr("d", d3.geoPath())
+  .attr("fill", "none")
+  .attr("stroke", "orange");
 
 // Source-Link
 container
